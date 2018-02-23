a888b4dc8942039e25d3e178dec5940bff3a80c3
Max Pavlov <max@radacode.com>
2018-02-23 12:01:39 +0200

    Adding attribute-based assembly redirect.

--- Radacode.PowerArm/Properties/AssemblyInfo.cs
+++ Radacode.PowerArm/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,6 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: ProvideBindingRedirection(AssemblyName = "System.Reflection.TypeExtensions",
+    NewVersion = "4.1.2.0", OldVersionLowerBound = "0.0.0.0",
+    OldVersionUpperBound = "4.1.2.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
